bc1eef8168bafc9f79181f8cea47f80215bfdc47
Tyler Maclachlan <tyler.maclachlan@mwrinfosecurity.com>
2020-04-06 09:46:29 +0200

    triangle

---
+++ src/main.rs
@@ -0,0 +1,127 @@
+#[macro_use]
+extern crate failure;
+extern crate gl;
+extern crate nalgebra;
+extern crate sdl2;
+extern crate vec_2_10_10_10;
+
+#[macro_use]
+extern crate render_gl_derive;
+
+pub mod render_gl;
+pub mod resources;
+mod triangle;
+
+use crate::resources::Resources;
+use nalgebra as na;
+use render_gl::data;
+use std::path::Path;
+
+#[derive(VertexAttribPointers, Copy, Clone, Debug)]
+#[repr(C, packed)]
+struct Vertex {
+    #[location = "0"]
+    pos: data::f32_f32_f32,
+    #[location = "1"]
+    clr: data::u2_u10_u10_u10_rev_float,
+}
+
+fn main() {
+    if let Err(e) = run() {
+        println!("{}", failure_to_string(e));
+    }
+}
+
+fn run() -> Result<(), failure::Error> {
+    let res = Resources::from_relative_exe_path(Path::new("assets")).unwrap();
+
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+
+    let gl_attr = video_subsystem.gl_attr();
+
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4, 1);
+
+    let window = video_subsystem
+        .window("Game", 900, 700)
+        .opengl()
+        .resizable()
+        .build()
+        .unwrap();
+
+    let _gl_context = window.gl_create_context().unwrap();
+    let gl = gl::Gl::load_with(|s| {
+        video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void
+    });
+
+    // set up shader program
+
+    let triangle = triangle::Triangle::new(&res, &gl)?;
+
+    let mut viewport = render_gl::Viewport::for_window(900, 700);
+    let color_buffer = render_gl::ColorBuffer::from_color(na::Vector3::new(0.3, 0.3, 0.5));
+
+    // main loop
+    viewport.set_used(&gl);
+    color_buffer.set_used(&gl);
+    let mut event_pump = sdl.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit { .. } => break 'main,
+                sdl2::event::Event::Window {
+                    win_event: sdl2::event::WindowEvent::Resized(w, h),
+                    ..
+                } => {
+                    viewport.update_size(w, h);
+                    viewport.set_used(&gl);
+                }
+                _ => {}
+            }
+        }
+
+        unsafe {
+            gl.Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        color_buffer.clear(&gl);
+        // draw triangle
+        triangle.render(&gl);
+
+        window.gl_swap_window();
+    }
+
+    Ok(())
+}
+
+pub fn failure_to_string(e: failure::Error) -> String {
+    use std::fmt::Write;
+
+    let mut result = String::new();
+
+    for (i, cause) in e
+        .iter_chain()
+        .collect::<Vec<_>>()
+        .into_iter()
+        .rev()
+        .enumerate()
+    {
+        if i > 0 {
+            let _ = writeln!(&mut result, "   Which caused the following issue:");
+        }
+        let _ = write!(&mut result, "{}", cause);
+        if let Some(backtrace) = cause.backtrace() {
+            let backtrace_str = format!("{}", backtrace);
+            if backtrace_str.len() > 0 {
+                let _ = writeln!(&mut result, " This happened at {}", backtrace);
+            } else {
+                let _ = writeln!(&mut result);
+            }
+        } else {
+            let _ = writeln!(&mut result);
+        }
+    }
+
+    result
+}
